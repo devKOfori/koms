2bc7fbb3c2abcbe19d6edd1124c0540de062cb2a
devKOfori <groupfivethree@gmail.com>
2025-05-30 17:14:13 +0300

    Update Department model

--- api/models.py
+++ api/models.py
@@ -28,9 +28,14 @@ class BaseModel(models.Model):
 
 class Department(BaseModel):
     name = models.CharField(max_length=255, unique=True, db_index=True)
+    description = models.TextField(blank=True, null=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name}"
 
     class Meta(BaseModel.Meta):
         db_table = "department"
